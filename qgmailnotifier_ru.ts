9cee785d54dec121000babe30e0b12c38e147528
Jacobtey <jacobtey@gmail.com>
2017-04-20 14:03:12 +0300

    Create qgmailnotifier_ru.ts
    Russian translation

---
+++ qgmailnotifier_ru.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru">
+<context>
+    <name>DialogOptions</name>
+    <message>
+        <location filename="dialog_options.ui" line="13"/>
+        <source>QGmailNotifier Options</source>
+        <translation>Настройки QGMailNotifier</translation>
+    </message>
+    <message>
+        <location filename="dialog_options.ui" line="29"/>
+        <source>Username:</source>
+        <translation>Логин:</translation>
+    </message>
+    <message>
+        <location filename="dialog_options.ui" line="39"/>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <location filename="dialog_options.ui" line="53"/>
+        <source>Check Interval:</source>
+        <translation>Интервал проверки:</translation>
+    </message>
+    <message>
+        <location filename="dialog_options.ui" line="73"/>
+        <source>Popup time span:</source>
+        <translation>Время показа сообщений:</translation>
+    </message>
+    <message>
+        <location filename="dialog_options.ui" line="90"/>
+        <source>Maximum Converations In Menu:</source>
+        <translation>Количество отображаемых сообщений:</translation>
+    </message>
+</context>
+<context>
+    <name>QGmailNotifier</name>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="82"/>
+        <source>View Inbox</source>
+        <translation>Открыть папку Входящие</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="83"/>
+        <source>Compose a Message</source>
+        <translation>Написать письмо</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="84"/>
+        <source>Check Mail Now</source>
+        <translation>Проверить почту</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="85"/>
+        <source>Tell me Again...</source>
+        <translation>Повторный показ сообщений</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="86"/>
+        <source>Options</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="87"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="92"/>
+        <source>Unread Conversations</source>
+        <translation>Непрочитанные</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="109"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="135"/>
+        <source>Reading feed</source>
+        <translation>Читать RSS</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="236"/>
+        <source>QGmailNotifier</source>
+        <translation>QGmailNotifier</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="237"/>
+        <source>QGmailNotifier version %1. Written by Evan Teran
+Icons are the trademark of Google
+
+Google, Gmail and Google Mail are registered trademarks of Google Inc.
+QGmailNotifier nor its author are in any way affiliated nor endorsed by Google Inc.
+</source>
+        <translation>Версия QGmailNotifier - %1. Автор - Evan Teran.
+Права на значки принадлежат Google.
+Google, Gmail и Google Mail - зарегистрированые товарные знаки Google Inc.
+Ни QGmailNotifier, ни его автор никак не связаны с Google Inc.</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="264"/>
+        <source>%1 unread conversation</source>
+        <translation>Непрочитанных писем - %1 шт.</translation>
+    </message>
+    <message>
+        <location filename="QGmailNotifier.cpp" line="266"/>
+        <source>%1 unread conversations</source>
+        <translation>Непрочитанных писем - %1 шт.</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="main.cpp" line="25"/>
+        <location filename="main.cpp" line="31"/>
+        <source>QGmailNotifier</source>
+        <translation type="unfinished">QGmailNotifier</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="25"/>
+        <source>I couldn&apos;t detect any system tray on this system.</source>
+        <translation type="unfinished">Системный лоток не обнаружен в системе.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="31"/>
+        <source>I couldn&apos;t detect any SSL supporton this system. Perhaps you need to rebuild QT with SSL support.</source>
+        <translation type="unfinished">Не обнаружена поддержка SSL в системе. Возможно нужно пересобрать QT с поддержкой SSL.</translation>
+    </message>
+</context>
+</TS>
